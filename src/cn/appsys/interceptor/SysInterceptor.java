a3d32467b4a0a6cf617913046e9b48f432e95f2c
1759862613 <1759862613@qq.com>
2019-07-15 08:40:53 +0800

    删除了一个文件

--- src/cn/appsys/interceptor/SysInterceptor.java
+++ src/cn/appsys/interceptor/SysInterceptor.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import cn.appsys.pojo.BackendUser;
@@ -13,10 +12,9 @@ import cn.appsys.tools.Constants;
 
 
 public class SysInterceptor extends HandlerInterceptorAdapter {
-	private Logger logger = Logger.getLogger(SysInterceptor.class);
 	
 	public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object handler) throws Exception{
-		logger.debug("SysInterceptor preHandle ==========================");
+	
 		HttpSession session = request.getSession();
 		
 		BackendUser backendUser = (BackendUser)session.getAttribute(Constants.USER_SESSION);
